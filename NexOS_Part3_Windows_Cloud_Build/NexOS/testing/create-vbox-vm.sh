c053e932bbd058a711bf65d96391e4ed890647c8
sidekicker777-glitch <sidekicker777@gmail.com>
2026-05-15 03:05:17 -0600

    Delete NexOS_Part3_Windows_Cloud_Build/NexOS directory

--- NexOS_Part3_Windows_Cloud_Build/NexOS/testing/create-vbox-vm.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Compatibility wrapper kept from Part 2. Prefer: make vbox-create
-set -Eeuo pipefail
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-exec "$SCRIPT_DIR/vbox-create.sh" "$@"
